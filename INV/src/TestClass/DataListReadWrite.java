082611b312a6e3cea9fcdffaa9c8462f161e758b
Andy Reeves <acreeves72@gmail.com>
2018-11-04 19:01:49 -0600

    Changes save and read format, added a static counter for prodNum
    two test classes to have proof of concept
    
    testFile.bin holds the value of the current working table

--- INV/src/TestClass/DataListReadWrite.java
+++ INV/src/TestClass/DataListReadWrite.java
@@ -2,7 +2,13 @@ package TestClass;
 
 import java.util.*;
 import java.io.*;
-
+/**
+ * 
+ * @author kingreebes
+ * Stores inventoryItem objects
+ * reads/writes them to file.
+ * Format of objects being saved is ArrayList<InventoryItem>
+ */
 public class DataListReadWrite
 {
 	private ArrayList<InventoryItem> list;
@@ -11,40 +17,40 @@ public class DataListReadWrite
 	public DataListReadWrite(String fileName) throws IOException 
 	{
 		this.file = fileName;
-		populateList();
+		this.list = new ArrayList<InventoryItem>();
+		this.populateList();
 	}
-	
+
 	public void populateList() throws IOException
 	{
 		try {
+			
 			FileInputStream fileIn = new FileInputStream(file);
 			ObjectInputStream objIn = new ObjectInputStream(fileIn);
-		
-		
-			while(objIn.readObject() != null)
-			{
-				list.add((InventoryItem)objIn.readObject());
-			}
+			this.list = (ArrayList<InventoryItem>)objIn.readObject();
 			objIn.close();
 			fileIn.close();
 		} catch (ClassNotFoundException e)
 		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			
 		}
+			catch(FileNotFoundException e) {
+				System.out.println("File not found.");
+				File createFile = new File(this.file);
+			}
 
 	}
-	public void writeList()
+	public void saveList()
 	{
 		try
 		{
 			FileOutputStream fileOut = new FileOutputStream(file);
 			ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
 			
-			for(InventoryItem item: list)
-			{
-				objOut.writeObject(item);
-			}
+			objOut.writeObject(list);
+			
 			objOut.close();
 			fileOut.close();
 		}
@@ -54,8 +60,7 @@ public class DataListReadWrite
 		}
 	}
 	public void updateList(InventoryItem item) {
-		list.add(item);
-		appendList(item);
+		this.list.add(item);
 	}
 	public void appendList(InventoryItem item)
 	{
@@ -77,6 +82,9 @@ public class DataListReadWrite
 	public void removeItem(int index)
 	{
 		list.remove(index);
-		writeList();
+	}
+	
+	public ArrayList<InventoryItem> getList(){
+		return this.list;
 	}
 }
